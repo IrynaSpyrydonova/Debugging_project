67543af950f3d6630e939ec4a349d495b5cefd0d
yauh-ask <yauheniya.askolkava@gmail.com>
2020-06-24 00:12:45 +0200

    reverse-string

--- scripts/reverse-a-string.js
+++ scripts/reverse-a-string.js
@@ -3,8 +3,14 @@
 console.log('-- loading: reverseString');
 
 
-function reverseString() { }
+function reverseString(str) { 
 
+  let reversed = '';
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversed += str[i];
+  }
+return reversed;
+}
 
 {
   console.log('-- testing: reverseString ');
